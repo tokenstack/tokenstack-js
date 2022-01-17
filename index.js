f9ab29a03336b6fc67eeef8632dba94fce9a827e
paulni123 <nitin.paul1530@gmail.com>
2022-01-17 01:11:55 -0500

    setup of wallet

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-const Test = () => {
-    return "Hello World"
-}
+import { ethers } from "ethers";
 
+class Tokenstack {
+    constructor(a, b) {
+        if (typeof (a) === "string") {
+            const userWallet = new ethers.Wallet(
+                a,
+                ethers.getDefaultProvider(b)
+            )
+            this.wallet = userWallet;
+            this.address = userWallet.address;
+        }
+        else {
+            const wallet = a;
+            this.wallet = wallet;
+            this.address = wallet.address;
+        }
+    }
+
+}
