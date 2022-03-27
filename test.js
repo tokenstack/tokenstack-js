defe62d0b56947b96b447d40f7c1549a0fea8828
Samrat Sahoo <44482993+SamratSahoo@users.noreply.github.com>
2022-03-27 09:29:52 -0400

    removed extra semicolon

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import { Tokenstack } from "./index.js";
 import 'dotenv/config';
 import * as fs from 'fs';
 
-const fileData = fs.readFileSync('./image.jpg', 'base64');; // Base64 Encoded Image
+const fileData = fs.readFileSync('./image.jpg', 'base64'); // Base64 Encoded Image
 const tokenstack = await new Tokenstack(process.env.TOKENSTACK_API_KEY, process.env.TOKENSTACK_PROJECT_ID);
 const nftTesting = await tokenstack.nft.mintNFT(fileData, process.env.TEST_PRIVATE_KEY, process.env.TEST_PUBLIC_KEY);
 
